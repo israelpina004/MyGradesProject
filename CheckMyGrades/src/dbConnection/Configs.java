c2ec178eeaec32a80787bd6eb4d5442c64c70289
israelpina004 <israelpi004@gmail.com>
2024-05-11 15:17:49 -0400

    Update configs.

--- CheckMyGrades/src/dbConnection/Configs.java
+++ CheckMyGrades/src/dbConnection/Configs.java
@@ -3,5 +3,5 @@ package dbConnection;
 public class Configs {
 	protected static String dbURL = "jdbc:mysql://localhost:3306/student_directory";
 	protected static String dbUser = "root";
-	protected static String dbPass = "RuPaNgO01!";
+	protected static String dbPass = "(USER SQL PASSWORD)";
 }
